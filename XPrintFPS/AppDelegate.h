c9eac4f5a2d7117eae07909954c7812ee0606281
StrongX <385135499@qq.com>
2016-03-31 00:58:47 +0800

    Initial Commit

---
+++ XPrintFPS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XPrintFPS
+//
+//  Created by xlx on 16/3/31.
+//  Copyright © 2016年 xlx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
